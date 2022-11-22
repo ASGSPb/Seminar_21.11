cee1bf6db86c5ddc0ba26e058aef7d11982311b1
ASGSPb <118271073+ASGSPb@users.noreply.github.com>
2022-11-22 18:46:29 +0300

    T02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, 
+// которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек 
+// в этой четверти (x и y).
+
+
+int  num = Convert.ToInt32(Console.ReadLine());
+
+if (num==1)
+{
+    Console.WriteLine("x>0 y>0");
+}
+else if (num==2)
+{
+    Console.WriteLine("x<0 y>0");
+}
+else if (num==3)
+{
+    Console.WriteLine("x<0 y<0");
+}
+else if (num==4)
+{
+    Console.WriteLine("x>0 y<0");
+}
+else
+{
+    Console.WriteLine("False");
+}
