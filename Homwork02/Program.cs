9d0e403bc19a69bcd1890f52817309895b275f32
ASGSPb <118271073+ASGSPb@users.noreply.github.com>
2022-11-22 23:49:36 +0300

    WH02

--- Homwork02/Program.cs
+++ Homwork02/Program.cs
@@ -5,6 +5,24 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+// Решение 1 простое
+
+// Console.Clear();
+// Console.WriteLine("Введите число");
+// Console.WriteLine("a = ");
+
+// int a = Convert.ToInt32(Console.ReadLine());
+// int c = Math.Abs ( a );
+// string b = Convert.ToString ( c );
+
+// if (b.Length < 3 )
+//     Console.WriteLine("Третьей цифры нет");
+       
+// Console.WriteLine( b[2]);
+
+
+// Решение 2 более интересное
+
 Console.Clear();
 Console.WriteLine("Введите число");
 Console.WriteLine("a = ");
