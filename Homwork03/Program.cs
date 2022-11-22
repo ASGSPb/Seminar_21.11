0e8f3e33920f1aa039a38f1cd0997a2549149378
ASGSPb <118271073+ASGSPb@users.noreply.github.com>
2022-11-23 00:56:32 +0300

    WH03

---
+++ Homwork03/Program.cs
@@ -0,0 +1,39 @@
+﻿ // Hапишите программу, 
+// которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+Console.WriteLine("Введите цифру от 1 до 7, обозначающую день недели");
+Console.WriteLine("a = ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int n =  array.Length;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == a){
+        if ( index == 5 ){
+            Console.WriteLine("Выходной");
+            break;
+        }
+        if( index == 6 ){
+            Console.WriteLine("Выходной");
+            break;
+        }
+        Console.WriteLine("Рабочий");
+        break;
+    }
+    index++;
+}  
+
+
+
+
